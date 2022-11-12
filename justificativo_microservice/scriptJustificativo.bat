2b42da0d51afa01da7915c688e2ed0002dbaf017
Daniel <daniel.catalan@usach.cl>
2022-11-11 21:51:45 -0300

    avance

---
+++ justificativo_microservice/scriptJustificativo.bat
@@ -0,0 +1,7 @@
+:: Actualizar justificativos
+call git add *
+call git commit -m "avance"
+call git push
+call mvnw clean install -DskipTests
+call docker build -t danaxar/justificativo-microservice .
+call docker push danaxar/justificativo-microservice
